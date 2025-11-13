216422ba18bb490c680eb5587771cb11846c294e
Jayeeeesh <jayeshthakur132@gmail.com>
2025-11-13 08:40:40 -0500

    add clear all task btn

--- todo_list.js
+++ todo_list.js
@@ -3,42 +3,51 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllTasksBtn = document.getElementById("clearAllTasksBtn");
 
 let tasks = [];
 
 function addTask() {
-            const taskText = taskInput.value.trim();
-            if (taskText !== "") {
-                tasks.push({ text: taskText});
-                taskInput.value = "";
-                displayTasks();
-            }
-        }
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText });
+        taskInput.value = "";
+        displayTasks();
+    }
+}
 
 
-        function displayTasks() {
-            taskList.innerHTML = "";
-            tasks.forEach((task, index) => {
-                const li = document.createElement("li");
-                li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
                     <label for="task-${index}">${task.text}</label>`;
-                li.querySelector("input").addEventListener("change", () => toggleTask(index));
-                taskList.appendChild(li);
-            });
-        }
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
 
-                 function toggleTask(index) {
-            tasks[index].completed = !tasks[index].completed;
-            displayTasks();
-        }
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
 
-            function clearCompletedTasks() {
-            tasks = tasks.filter(task => !task.completed);
-            displayTasks();
-        }
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+function clearAllTasks() {
+    tasks = [];  
+    displayTasks();  
+}
 
 
-        addTaskBtn.addEventListener("click", addTask);
+
+
+addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click",clearAllTasks);
 
- displayTasks();
+displayTasks();
